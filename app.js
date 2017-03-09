b993c812dbf599c071a3821156e3a51d8cba45fc
Fion Pang <fipa0809@gmail.com>
2017-03-09 00:09:43 -0800

    second commit

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ $(document).ready(function() {
 			q: artistName,
 			key: '344c535a8dd24b82a640784c365cc416'
 		}
-	});
-	var dataFromApi = $.getJSON(spotifyBaseURL, params);
+
+		var dataFromApi = $.getJSON(spotifyBaseURL, params);
 	console.log(dataFromApi);
 
 	var getTopTracks = dataFromApi.then(function(data) {
@@ -34,78 +34,85 @@ $(document).ready(function() {
 			 "'s Top 5 Tracks");
 // get top tracks
 
-			return $.getJSON(spotifyArtists + artistId + spotifyTopTracks);
-		}
-	});
+			var getData = $.getJSON(spotifyArtists + artistId + spotifyTopTracks);
+//append tracks
 
-
-// append tracks
-
-	var trackList = [];
-	getTopTracks.then(function(data) {
+			var trackList = [];
+	getData.then(function(data) {
 		console.log(data.tracks[0].artists[0].name);
 		var artist = data.tracks[0].artists[0].name;
 
 		for (var i = 0; i < 5; i++) {
 			trackList.push(data.tracks[i].name);
-			$('ul').append('<li class="trackItem" style="display: none;" id=' + 
-				data.tracks[i].id + '>' + data.tracks[i].name + '</li>');
+			$('ul').append('<li style="list-style:none" class="trackItem"'+ '>' + data.tracks[i].name + ' <button type="text"' +
+				' class="play-button" id="' + data.tracks[i].id + '">Play</button></li>');
 		}
 		console.log(trackList);
+
+	// preview tracks
+
+		var audioList = [];
+
+		var getAudio = function () {
+			var trackId = $(this).attr("id");
+		    var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
+		    var playTrack = track.then(function(playTrack) {
+		    	var playAudio = new Audio(playTrack.preview_url);
+		    	console.log(playTrack.preview_url);
+
+		    	audioList = [];
+		        audioList.push(playAudio);
+		        playAudio.play();
+		    });
+		};
+
+	// checks if anything in array, if there is, pause and clear it to play next song
+
+		$('ul').on('click', '.play-button', function() {
+		    if (audioList.length === 1) {
+		    	audioList[0].pause(); 
+
+		    	var trackId = $(this).attr("id");
+		      	var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
+		      	var playTrack = track.then(function(playTrack) {
+		        	var playAudio = new Audio(playTrack.preview_url);
+		        	console.log(playTrack.preview_url);
+
+		        	audioList = [];
+		        	audioList.push(playAudio);
+		        	playAudio.play();
+		      	});
+		  	}
+		
+	// if nothing is in the array play normally
+		 
+		    else {
+		      	var trackId = $(this).attr("id");
+		      	var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
+		      	var playTrack = track.then(function(playTrack) {
+		        	var playAudio = new Audio(playTrack.preview_url);
+		        	console.log(playTrack.preview_url);
+	// adds current song into the audiolist to check if something is in array later.
+
+		        	audioList = [];
+		        	audioList.push(playAudio);
+		        	playAudio.play();
+		      	});
+		    } 
+	  	});
+
+
+		});
+		$('.search-text').val("");
+			}
+
+		});
+
+
+
+
+
 	});
-	$('.search-text').val("");
-
-
-// preview tracks
-
-	var audioList = [];
-
-	var getAudio = function () {
-		var trackId = $(this).attr("id");
-	    var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
-	    var playTrack = track.then(function(playTrack) {
-	    	var playAudio = new Audio(playTrack.preview_url);
-	    	console.log(playTrack.preview_url);
-
-	    	audioList = [];
-	        audioList.push(playAudio);
-	        playAudio.play();
-	    });
-	};
-
-// checks if anything in array, if there is, pause and clear it to play next song
-
-	$('ul').on('click touchstart', '.trackItem', function() {
-	    if (audioList.length === 1) {
-	    	audioList[0].pause(); 
-
-	    	var trackId = $(this).attr("id");
-	      	var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
-	      	var playTrack = track.then(function(playTrack) {
-	        	var playAudio = new Audio(playTrack.preview_url);
-	        	console.log(playTrack.preview_url);
-
-	        	audioList = [];
-	        	audioList.push(playAudio);
-	        	playAudio.play();
-	      	});
-	  	}
-// if nothing is in the array play normally
-	 
-	    else {
-	      	var trackId = $(this).attr("id");
-	      	var track = $.getJSON('https://api.spotify.com/v1/tracks/' + trackId);
-	      	var playTrack = track.then(function(playTrack) {
-	        	var playAudio = new Audio(playTrack.preview_url);
-	        	console.log(playTrack.preview_url);
-// adds current song into the audiolist to check if something is in array later.
-
-	        	audioList = [];
-	        	audioList.push(playAudio);
-	        	playAudio.play();
-	      	});
-	    } 
-  	});
-
+	
 
 });
